19437d903f5afdf5695b8331af03595838b6ca62
vktadm <tarnavskayavictoria@gmail.com>
2025-01-19 17:31:32 +0300

    Solve 290

--- Hashmap/easy/_290_Word_Pattern.py
+++ Hashmap/easy/_290_Word_Pattern.py
@@ -13,3 +13,22 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        s = s.split()
+
+        if len(pattern) != len(s):
+            return False
+
+        pattern_map = {}
+        s_map = []
+
+        for num in range(len(pattern)):
+            if pattern[num] in pattern_map:
+                if pattern_map[pattern[num]] != s[num]:
+                    return False
+            else:
+                if s[num] in s_map:
+                    return False
+                else:
+                    pattern_map[pattern[num]] = s[num]
+                    s_map.append(s[num])
+        return True
--- Hashmap/easy/tests/_290_Word_Pattern_test.py
+++ Hashmap/easy/tests/_290_Word_Pattern_test.py
@@ -5,22 +5,20 @@ from Hashmap.easy._290_Word_Pattern import Solution
 
 class SolutionTest(unittest.TestCase):
 
-    def library_solve(self, pattern, s):
-        hashmap = list(zip(list(pattern), s.split()))
-
-
-    def run_test(self, ransom_note, magazine):
-        print(self.library_solve(ransom_note, magazine))
-        # self.assertEqual(self.library_solve(ransom_note, magazine), Solution().wordPattern(ransom_note, magazine))
+    def run_test(self, test_value, ransom_note, magazine):
+        self.assertEqual(test_value, Solution().wordPattern(ransom_note, magazine))
 
     def test_case_1(self):
-        self.run_test("abba", "dog cat cat dog")
+        self.run_test(True, "abba", "dog cat cat dog")
 
     def test_case_2(self):
-        self.run_test("abba", "dog cat cat fish")
+        self.run_test(False, "abba", "dog cat cat fish")
 
     def test_case_3(self):
-        self.run_test("aaaa", "dog cat cat dog")
+        self.run_test(False, "aaaa", "dog cat cat dog")
+
+    def test_case_4(self):
+        self.run_test(False, "aaa", "aa aa aa aa")
 
 
 if __name__ == "__main__":
