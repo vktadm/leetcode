c371092e8a7d305613923f2f8f840ddf8cab8837
vktadm <tarnavskayavictoria@gmail.com>
2025-01-19 17:58:59 +0300

    Solve 242

---
+++ Hashmap/easy/_242_Valid_Anagram.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s) != len(t):
+            return False
+
+        for i in set(s):
+            if s.count(i) != t.count(i):
+                return False
+        return True
+
+s = Solution()
+print(s.isAnagram("anagham", "nagaram"))
